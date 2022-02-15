faf1ad1b022ba67edb34cc9d67938831b20204c2
Ubuntu <ubuntu@ip-172-31-26-142.ec2.internal>
2022-02-15 20:53:31 +0000

    main

---
+++ src/HelloWorld.ts
@@ -0,0 +1,12 @@
+/*
+* This program is HelloWorld
+*
+* @author  Abdul Basit Butt
+* @version 1.0
+* @since   2022-02-15
+*/
+
+console.log("Hello, World!")
+console.log("")
+console.log("Done.");
+
